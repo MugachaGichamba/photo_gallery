75dc94d2c33e06ab03bf70f8d94dbabbc124f7d9
MugachaGichamba <mugachadjango@gmail.com>
2019-05-14 11:38:08 +0300

    category form done

--- galleria/urls.py
+++ galleria/urls.py
@@ -18,13 +18,14 @@ from django.urls import path
 from photos import views
 from django.conf import settings
 from django.conf.urls.static import static
+from photos.views import PicListView, Location
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='gallery-home'),
+    path('', PicListView.as_view(), name='gallery-home'),
+    # path('category/<string:category>/', CategoryView.as_view(), name='image_detail'),
+    path('location/', Location, name="get_location"),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Image
+from .models import Image, Location
+from django.views.generic import ListView, DetailView
+
 
 def home(request):
     context = {
@@ -7,3 +9,23 @@ def home(request):
     }
     return render(request, 'photos/home.html', context)
 
+
+class PicListView(ListView):
+    model = Image
+    template_name = 'photos/home.html'
+    context_object_name = "images"
+
+
+def Location(request):
+    data = request.GET["Location"]
+
+    # context = {"images" : Image.objects.filter(image_location=data)}
+    for image in Image.objects.filter():
+        print(image.image_category)
+
+    return render(request, 'location.html', {'data':data})
+
+#
+# class CategoryView(DetailView):
+#     model = Image
+#
